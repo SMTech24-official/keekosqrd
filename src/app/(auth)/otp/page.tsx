88e0ea223c8f26ad36ba41c98de5f0990020a290
nirob019207 <nirobhasanbd01955@gmail.com>
2025-01-18 09:32:34 +0600

    .

--- src/app/(auth)/otp/page.tsx
+++ src/app/(auth)/otp/page.tsx
@@ -1,12 +1,11 @@
-
-import OtpPage from '@/components/otppage/OtpPage';
-import React from 'react';
+import OtpPage from "@/components/auth/otppage/OtpPage";
+import React from "react";
 const page = () => {
-    return (
-        <div>
-        <OtpPage></OtpPage>
-        </div>
-    );
+  return (
+    <div>
+      <OtpPage></OtpPage>
+    </div>
+  );
 };
 
 export default page;
